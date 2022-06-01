a42592fc07a36be4e7f78eb8fb0ce4b7269fa3c0
shivaiitb21 <shivnalgire4@gmail.com>
2022-06-01 12:07:51 +0530

    time format conversion

---
+++ timeconvert.py
@@ -0,0 +1,18 @@
+def timeConversion(s):
+    # Write your code here
+    hh=int(s[:2])
+    if "PM" in s:
+        if hh!=12:
+            hh=hh+12
+
+    if "AM" in s:
+        if hh==12:
+            hh=00
+            
+    hour=(str(hh)).zfill(2)
+    
+    return f"{hour}{s[2:-2]}"
+
+s = "07:25:00PM"
+
+print(timeConversion(s))
