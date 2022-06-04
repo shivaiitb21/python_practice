f254432ff8ad7daf8a2f7c43351bde9e3c687ef6
shivaiitb21 <shivnalgire4@gmail.com>
2022-06-04 23:43:54 +0530

    xor

---
+++ count_in_array.py
@@ -0,0 +1,7 @@
+def lonelyinteger(a):
+    
+    for n in a:
+        if a.count(n)==1:
+            return n 
+
+#Count number of instance of n in array a
---
+++ xor.py
@@ -0,0 +1,7 @@
+
+def lonelyinteger(a):
+    # Write your code here
+    unique=0
+    for i in a:
+        unique^= i
+    return unique
