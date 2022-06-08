5d82ed904f156fbd4e96adb0f695ffdc81315d97
shivaiitb21 <shivnalgire4@gmail.com>
2022-06-08 15:25:52 +0530

    finding pairs

---
+++ finding _pairs.py
@@ -0,0 +1,13 @@
+def sockMerchant(n, ar):
+    # Write your code here
+    counter=0
+    ar_set=list(set(ar))
+    for i in ar_set:
+        if ar.count(i)%2==0:
+            m = int(ar.count(i)/2)
+            counter = counter + m
+        else:
+            l = int((ar.count(i)-1)/2)
+            counter = counter +l
+            
+    return counter
