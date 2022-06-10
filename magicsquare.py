6e03cc54ceb716116c431f85600dc4c3ef35e511
shivaiitb21 <shivnalgire4@gmail.com>
2022-06-10 15:40:13 +0530

    matrix conversion

---
+++ magicsquare.py
@@ -0,0 +1,23 @@
+def formingMagicSquare(s):
+    # Write your code here
+    possible_magic_squares = [
+            [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+            [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+            [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+            [[2, 9, 4], [7, 5, 3], [6, 1, 8]], 
+            [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+            [[4, 3, 8], [9, 5, 1], [2, 7, 6]], 
+            [[6, 7, 2], [1, 5, 9], [8, 3, 4]], 
+            [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+    ]
+    mincost = float('inf')
+    for p in possible_magic_squares:
+        cur_cost=0
+        for i in range(len(p)):
+            for j in range(len(p[i])):
+                cur_cost += abs(s[i][j]-p[i][j])
+        if cur_cost<mincost:
+            mincost=cur_cost 
+    
+    return mincost
+                
