d5186b715c4e9da0ec0d8c016921dd08f4f961b9
shivaiitb21 <shivnalgire4@gmail.com>
2022-06-28 00:11:04 +0530

    added time check

--- findstr.py
+++ findstr.py
@@ -1,2 +1,10 @@
+import time
+start = time.time()
+
 def count_substring(s, sub):
+
     return [s[i:i+len(sub)] for i in range(len(s))].count(sub)
+
+print(count_substring("ABCDCDC","CDC"))
+end = time.time()
+print(end-start)
