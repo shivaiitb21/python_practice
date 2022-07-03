152c4011b64abda0cb2ce9e13513c3914468df6c
Shivanand Nalgire <62739431+shivaiitb21@users.noreply.github.com>
2022-07-03 23:34:21 +0530

    number convrt

---
+++ numberconvert.py
@@ -0,0 +1,11 @@
+number = 5
+print("{0:o}".format(number))
+print("{0:b}".format(number))
+# print("{0:d}".format(number))
+print("{0:X}".format(number))
+
+print(oct(number))
+print(bin(number))
+print(hex(number))
+
+#Formatting using first method will remove the initials i.e. hex or dec or oct
