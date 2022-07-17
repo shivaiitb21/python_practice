2e08e067c15bec51009ca661155e074de255f979
Shivanand Nalgire <62739431+shivaiitb21@users.noreply.github.com>
2022-07-17 20:41:43 +0530

    Create prntfmt.py

---
+++ prntfmt.py
@@ -0,0 +1,12 @@
+txt1 = "My name is {fname}, I'm {age}".format(fname = "John", age = 36)
+txt2 = "My name is {0}, I'm {1}".format("John",36)
+txt3 = "My name is {}, I'm {}".format("John",36)
+
+txt = "For only {0:.2f} dollars!"
+print(txt.format(49))
+
+txt = "For only {:.2f} dollars!"
+print(txt.format(49))
+
+print("{:b}".format(2))
+# Formatting examples
